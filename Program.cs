acb5ffceffabca78324bab7c87c30db450071a3e
Josh Mann <jlmann72@gmail.com>
2020-11-20 16:24:42 -0500

    Added Search

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 
 namespace TestOneArray
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int[] scores = { 90, 70, 50, 80, 60, 85 };
+            Console.Write("Please enter the value you want to search: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            bool isSearch = false;
+
+            for (int index = 0; index < scores.Length; index++)
+            {
+                if (scores[index] == value)
+                {
+                    isSearch = true;
+                    Console.Write($"Found value: {value} the index is: {index}");
+                    break;
+                }
+            }
+
+            if (!isSearch)
+            {
+                Console.Write($"The value {value} was not found");
+            }
+        }
     }
-  }
 }
